84c272fd730aa78f49a8ce3bc264a5d221649bcd
Sergiu Waxmann <waxmann.sergiu@me.com>
2020-10-27 22:30:25 +0100

    Initial Commit

---
+++ tic-tac-toe-game/tic_tac_toe_gameApp.swift
@@ -0,0 +1,17 @@
+//
+//  tic_tac_toe_gameApp.swift
+//  tic-tac-toe-game
+//
+//  Created by Sergiu Waxmann on 27/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct tic_tac_toe_gameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
